30ae840624226cd8c54a66ff1d6376e395aef8a2
Alexander Nestserov <alexerdeader@gmail.com>
2021-10-16 23:53:40 +0300

    lesson11+

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
+'use strict';
 
+const square = document.getElementById('square');
+const btn = document.getElementById('btn');
+const text = document.getElementById('text');
+const logger = function () {
+   let a = text.value;
+   square.style.backgroundColor = text.value;
+   text.style.backgroundColor = text.value;
+};
+btn.addEventListener('click', logger);//task1
 
+const ebtn = document.getElementById('e_btn');
+ebtn.style.display = 'none';//task2
 
-
+const circle = document.getElementById('circle');
+const range = document.getElementById('range');
+const span = document.getElementById('range-span');
+const log = function (event) {
+   span.textContent = event.target.value;
+   circle.style.width = event.target.value + '%';
+   circle.style.height = event.target.value + '%';
+};
+range.addEventListener('input', log);
+range.addEventListener('change', log);//task3
 
